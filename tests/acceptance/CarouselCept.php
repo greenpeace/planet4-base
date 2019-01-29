87faf2afb01cfccf55b0b28c2ce361ae5674bf60
Pablo Cubico <pablocuadrado@gmail.com>
2019-01-29 12:55:46 -0300

    Add more specific selectors.

--- tests/acceptance/CarouselCept.php
+++ tests/acceptance/CarouselCept.php
@@ -9,20 +9,20 @@ $I->wantTo('check carousel is rendered correctly');
 $I->amOnPage('/');
 
 // scroll down or the cookie banner covers the "next" button
-$I->scrollTo('.carousel-item-container');
+$I->scrollTo('.carousel-wrap .carousel-item-container');
 
 // we see the first image
-$I->seeElement('.carousel-item.active img', [
+$I->seeElement('.carousel-wrap .carousel-item.active img', [
 	'src' => 'http://www.planet4.test/wp-content/uploads/2018/05/af565f01-gp0stpr4h_web_size_with_credit_line.jpg']
 );
 
 // click the next link
-$I->click('.carousel-control-next-icon');
+$I->click('.carousel-wrap .carousel-control-next-icon');
 
 // Wair for the scrolling to finish and the .active class being appended to the second image
-$I->wait(2);
+$I->wait(1);
 
 // and see the next image :)
-$I->seeElement('.carousel-item.active img', [
+$I->seeElement('.carousel-wrap .carousel-item.active img', [
 	'src' => 'http://www.planet4.test/wp-content/uploads/2018/05/ea8f1af6-gp0stq27a_web_size_with_credit_line.jpg']
 );
