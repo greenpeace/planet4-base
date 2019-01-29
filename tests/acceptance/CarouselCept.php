27d5fb889158529a69eeda44f2f8568c7fa12231
Pablo Cubico <pablocuadrado@gmail.com>
2019-01-29 11:18:17 -0300

    Wait for the active class in the second slide element.

--- tests/acceptance/CarouselCept.php
+++ tests/acceptance/CarouselCept.php
@@ -9,17 +9,18 @@ $I->wantTo('check carousel is rendered correctly');
 $I->amOnPage('/');
 
 // scroll down or the cookie banner covers the "next" button
-$I->scrollTo('.carousel-wrap');
+$I->scrollTo('.carousel-item-container');
 
 // we see the first image
 $I->seeElement('.carousel-item.active img', [
 	'src' => 'http://www.planet4.test/wp-content/uploads/2018/05/af565f01-gp0stpr4h_web_size_with_credit_line.jpg']
 );
 
-// click the next icon
+// click the next link
 $I->click('.carousel-control-next-icon');
 
-$I->wait(1);
+// Wair for the scrolling to finish and the .active class being appended to the second image
+$I->wait(2);
 
 // and see the next image :)
 $I->seeElement('.carousel-item.active img', [
