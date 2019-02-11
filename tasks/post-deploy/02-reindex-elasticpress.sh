df4c48f2b12bb9579425bfde50d831580a114558
Angelos Theodorakopoulos <angtheod@gmail.com>
2019-02-11 16:49:22 +0200

    Set es host url, activate document indexing, perform indexing and show detailed message if a post fails to index.

--- tasks/post-deploy/02-reindex-elasticpress.sh
+++ tasks/post-deploy/02-reindex-elasticpress.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-# wp elasticpress index
+wp config set --add --type=constant EP_HOST http://p4-es-elasticsearch-client.default.svc.cluster.local:9200
+wp elasticpress activate-feature documents
+wp elasticpress index --show-bulk-errors
