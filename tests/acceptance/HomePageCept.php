d4949f7c4f82c265e3b2255cf3158ad606dc797f
Pablo Cubico <pablocuadrado@gmail.com>
2019-02-12 09:19:52 -0300

    Fix CSS selector.

--- tests/acceptance/HomePageCept.php
+++ tests/acceptance/HomePageCept.php
@@ -8,7 +8,7 @@ $I->see('People Power', 'h2');
 $I->see('Change the world', 'h2');
 
 // Ensure the country dropdown opens
-$I->click('country-dropdown-toggle');
+$I->click('.country-dropdown-toggle');
 
 $I->seeElement('.country-list.open');
 
