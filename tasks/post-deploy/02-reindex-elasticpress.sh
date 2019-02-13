3890bb414f0edc9ceb42c81cd6f89c71d2fc9ede
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-02-13 10:36:51 +0200

    Removing elasticpress post deploy scripts until we debug
    They make several deploys (New Zealand, Handbook) fail

--- tasks/post-deploy/02-reindex-elasticpress.sh
+++ tasks/post-deploy/02-reindex-elasticpress.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wp config set --add --type=constant EP_HOST http://p4-es-elasticsearch-client.default.svc.cluster.local:9200
-wp elasticpress activate-feature documents
-wp elasticpress index --show-bulk-errors
+# wp config set --add --type=constant EP_HOST http://p4-es-elasticsearch-client.default.svc.cluster.local:9200
+# wp elasticpress activate-feature documents
+# wp elasticpress index --show-bulk-errors
