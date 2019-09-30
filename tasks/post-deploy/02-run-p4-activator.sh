d0dcb880ecc04cb8a9bf2b644893c2f64b512904
Nikos Roussos <nikos@roussos.cc>
2019-09-30 16:17:12 +0300

    Revert "PLANET-4050 Add post deploy script to run planet4 master theme activator"
    This reverts commit 4b6eb55e5c4c1299cf8dd620c3d22cde47f36643.

--- tasks/post-deploy/02-run-p4-activator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Instantiate P4_Activator and run its functions.
-# This will trigger planet4-master-theme activation/deactivation hooks.
-# It is meant for running one-off actions like adding custom roles and capabilities.
-wp eval '(new P4_Activator())->run();'
