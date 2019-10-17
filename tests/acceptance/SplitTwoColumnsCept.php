b2f8fb95e1637cae3f72b1ced3107ccb7cdfab69
Nikos Roussos <nikos@roussos.cc>
2019-10-17 16:23:00 +0300

    SplitTwoColumns: Make assertions more explicit

--- tests/acceptance/SplitTwoColumnsCept.php
+++ tests/acceptance/SplitTwoColumnsCept.php
@@ -26,8 +26,8 @@ $I->amOnPage('/' . $slug);
 // Check the Split Two Column block
 $I->scrollTo('.split-two-column');
 $I->see('Issue', 'h2.split-two-column-item-title');
-$I->see('We fight', 'p.split-two-column-item-subtitle');
+$I->see('We fight', 'div.item--left p.split-two-column-item-subtitle');
 $I->see('Read more', 'a.split-two-column-item-link');
 $I->see('#Climate', 'a.split-two-column-item-tag');
-$I->see('Campaign description', 'p.split-two-column-item-subtitle');
+$I->see('Campaign description', 'div.item--right p.split-two-column-item-subtitle');
 $I->see('Get Involved', 'a.split-two-column-item-button');
