8aebb0c13bf87eaa329166ffc308e4c7f3efa9e0
Nikos Roussos <nikos@roussos.cc>
2019-10-18 13:07:24 +0300

    Update HomePage test to reflect default content

--- tests/acceptance/HomePageCept.php
+++ tests/acceptance/HomePageCept.php
@@ -4,11 +4,6 @@ $I->wantTo('check home page');
 
 $I->amOnPage('/');
 
-$I->scrollTo('.columns-block');
-
-$I->see('People Power', 'h3');
-$I->see('Change the world', 'h3');
-
 // Ensure the country dropdown opens
 $I->click('.country-dropdown-toggle');
 
