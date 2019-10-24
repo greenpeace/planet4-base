87b5259827bc6e994b2da0c22aff10c4af6e015c
Nikos Roussos <nikos@roussos.cc>
2019-10-24 12:58:57 +0300

    Add js conversion steps

--- tasks/post-deploy/05-run-shortcodes-to-gutenberg-conversion.sh
+++ tasks/post-deploy/05-run-shortcodes-to-gutenberg-conversion.sh
@@ -5,3 +5,28 @@ wp cache flush
 wp p4-gblocks convert_to_gutenberg
 wp plugin deactivate planet4-plugin-blocks
 wp cache flush
+
+# Install dependencies for nodejs
+apt update
+apt install gnupg -y
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+apt install nodejs -y
+
+# Clone convert-to-gutenberg repo, install npm packages
+cd /tmp
+git clone https://github.com/greenpeace/planet4-convert-to-gutenberg
+cd planet4-convert-to-gutenberg
+sed 's|/path/to/wordpress/public/|/app/source/public|g' config.example.json > config.json
+npm install
+
+#  Run core block gutenberg conversion
+node convert-blocks.js page
+wp cache flush
+node convert-blocks.js post
+wp cache flush
+node convert-blocks.js campaign
+wp cache flush
+
+# Clean up
+apt remove nodejs -y
+cd /tmp && apt-get clean && rm -Rf /tmp/* /var/tmp/* /var/lib/apt/lists/*
