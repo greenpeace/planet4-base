b529728fca1415a547050e58d7112161089d8016
Nikos Roussos <nikos@roussos.cc>
2019-10-29 14:50:54 +0200

    Remove js conversion for now

--- tasks/post-deploy/04-run-shortcodes-to-gutenberg-conversion.sh
+++ tasks/post-deploy/04-run-shortcodes-to-gutenberg-conversion.sh
@@ -18,29 +18,3 @@ wp cache flush
 wp p4-gblocks convert_to_gutenberg
 wp plugin deactivate planet4-plugin-blocks
 wp cache flush
-
-# Install dependencies for nodejs
-apt-get update
-apt-get install gnupg -y
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
-apt-get install nodejs -y
-
-# Clone convert-to-gutenberg repo, install npm packages
-cd /tmp || exit
-git clone https://github.com/greenpeace/planet4-convert-to-gutenberg
-cd planet4-convert-to-gutenberg || exit
-sed 's|/path/to/wordpress/public/|/app/source/public|g' config.example.json > config.json
-npm install
-
-#  Run core block gutenberg conversion
-node convert-blocks.js page
-wp cache flush
-node convert-blocks.js post
-wp cache flush
-node convert-blocks.js campaign
-wp cache flush
-
-# Clean up
-apt-get remove nodejs gnupg -y
-apt-get autoremove -y
-apt-get clean
