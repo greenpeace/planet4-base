63d292e5e604a7a53f0e0eb10cac54db5ad380b8
Nikos Roussos <nikos@roussos.cc>
2019-11-06 18:00:24 +0200

    Remove post-deploy scripts that are no longer needed
    * Users sync
    * Gutenberg conversion
    * Gutenberg editor

--- tasks/post-deploy/03-enable_gutenberg_editor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Set gutenberg editor as the default editor
-wp option update classic-editor-replace 'block'
-
-# Allow users to switch editors (classic - gutenberg)
-wp option update classic-editor-allow-users 'disallow'
