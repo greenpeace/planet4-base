9bd2c000e901181b2e38254c60ae68c3de26b4c4
Nikos Roussos <nikos@roussos.cc>
2020-01-22 13:07:46 +0200

    Remove obsolete preconnect script

--- tasks/post-deploy/04-add-default-preconnect-domains.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Set domains for the Preconnect Settings option
-echo -e "https://in.hotjar.com\nhttps://act.greenpeace.org" > domains.txt
-wp option patch insert planet4_options preconnect_domains < domains.txt
-rm domains.txt
