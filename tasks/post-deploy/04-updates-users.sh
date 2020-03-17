a6441a4aaf13b6a725382da4b1f7528cd9f120dd
Nikos Roussos <nikos@roussos.cc>
2020-03-17 13:48:18 +0200

    Demote admin user on all instances

--- tasks/post-deploy/04-updates-users.sh
+++ tasks/post-deploy/04-updates-users.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-wp user delete atheodor@greenpeace.org --reassign=1 --yes
-wp user delete kdiamant@greenpeace.org --reassign=1 --yes
-wp user delete koyandianthos@gmail.com --reassign=1 --yes
-wp user delete kkokkoro@greenpeace.org --reassign=1 --yes
-wp user delete dpivo@greenpeace.org --reassign=1 --yes
+wp user update admin --role=subscriber --user_email=noreply@greenpeace.org
