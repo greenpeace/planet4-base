766d4abe5806f9cea4325ac918fa157c4e6aadfb
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2020-05-19 22:35:50 +0530

    PLANET-5099 Fix post-deploy script namespace issue

--- tasks/post-deploy/01-run-p4-activator.sh
+++ tasks/post-deploy/01-run-p4-activator.sh
@@ -5,7 +5,7 @@
 # It is meant for running one-off actions like adding custom roles and capabilities.
 if ( wp theme is-active planet4-master-theme ) || ( wp theme status | grep -q 'P planet4-master-theme' )  then
   echo "Run P4_Activator"
-  wp eval '(new P4_Activator())->run();'
+  wp eval '(new P4MT\P4_Activator())->run();'
 else
   echo "Skip running P4_Activator, neither planet4-master-theme nor one of its child themes is activated."
 fi
