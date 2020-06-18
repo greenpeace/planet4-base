29d211762607b67dc24d60df78a8ae3a105e60c8
inwerpsel <pieter.vincent1988@gmail.com>
2020-06-18 19:01:47 +0200

    PLANET-5099 Use command instead of eval

--- tasks/post-deploy/01-run-p4-activator.sh
+++ tasks/post-deploy/01-run-p4-activator.sh
@@ -5,7 +5,7 @@
 # It is meant for running one-off actions like adding custom roles and capabilities.
 if ( wp theme is-active planet4-master-theme ) || ( wp theme status | grep -q 'P planet4-master-theme' )  then
   echo "Run P4_Activator"
-  wp eval '(new P4_Activator())->run();'
+  wp p4-run-activator
 else
   echo "Skip running P4_Activator, neither planet4-master-theme nor one of its child themes is activated."
 fi
