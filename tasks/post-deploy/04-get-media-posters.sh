89ce3450d0b0200007833ac73c1d0797a54f5842
Nikos Roussos <nikos@roussos.cc>
2020-10-27 21:18:53 +0200

    Remove obsolete script

--- tasks/post-deploy/04-get-media-posters.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Get posts that use the media block and have a poster
-wp db query "SELECT ID, post_title FROM wp_posts WHERE post_status = 'publish'AND post_content LIKE '%video_poster_img%'"
