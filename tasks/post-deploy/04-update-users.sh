4880ef0aa5d8c270b6135e29ade68ad129af47f1
Nikos Roussos <nikos@roussos.cc>
2020-11-10 20:49:53 +0200

    Remove user script

--- tasks/post-deploy/04-update-users.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wp user create mfatome mfatome@greenpeace.org --first_name="Magali" --last_name="Fatome - P4 team" --role=administrator --porcelain || true
-wp user delete wmorrisj --reassign=1 --yes
