7e3ab703ec260959123a1371ffcccc6c40c27664
Nikos Roussos <nikos@roussos.cc>
2020-11-15 17:09:26 +0200

    PLANET-5584 Hardcode gp domain to wp-stateless domain
    This will enable two things:
    1. Use one worker route for all environments
    2. Use the same worker for all subdomains (eg. handbook, storytelling)

--- tasks/post-deploy/04-stateless-static.sh
+++ tasks/post-deploy/04-stateless-static.sh
@@ -3,10 +3,10 @@
 # Make sure we exclude NROs on other TLDs
 if [[ "$APP_HOSTNAME" == *"greenpeace.org"* ]]; then
   # Set custom wp-stateless domain
-  wp option update sm_custom_domain "https://${APP_HOSTNAME}/static/${WP_STATELESS_MEDIA_BUCKET}/"
+  wp option update sm_custom_domain "https://www.greenpeace.org/static/${WP_STATELESS_MEDIA_BUCKET}/"
 
   # Search and replace existing assets
-  wp search-replace "https://storage.googleapis.com/${WP_STATELESS_MEDIA_BUCKET}/" "https://${APP_HOSTNAME}/static/${WP_STATELESS_MEDIA_BUCKET}/" --precise --skip-columns=guid
+  wp search-replace "https://storage.googleapis.com/${WP_STATELESS_MEDIA_BUCKET}/" "https://www.greenpeace.org/static/${WP_STATELESS_MEDIA_BUCKET}/" --precise --skip-columns=guid
 
   # Disable CF Image Optimization
   wp option patch delete planet4_options cloudflare_img_opt || true
