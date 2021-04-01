fb75a47667c5831c515131a7e3e013aee0cfcb54
Nikos Roussos <nikos@roussos.cc>
2021-04-01 09:57:06 +0300

    Update ES host path

--- tasks/post-deploy/07-es-host.sh
+++ tasks/post-deploy/07-es-host.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$APP_ENVIRONMENT" = "development" ]; then
-  wp option update ep_host "http://elasticsearch-client.elastic.svc.cluster.local:9200/"
+  wp option update ep_host "http://elasticsearch-data.elastic.svc.cluster.local:9200/"
 fi
