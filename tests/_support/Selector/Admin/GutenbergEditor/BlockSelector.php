22181614d169d48ab8964eb4ef575e072aace889
Florent Hernandez <florent.hernandez@greenpeace.org>
2021-05-26 13:11:20 +0200

    Fix tests errors on BlockSelector enum

--- tests/_support/Selector/Admin/GutenbergEditor/BlockSelector.php
+++ tests/_support/Selector/Admin/GutenbergEditor/BlockSelector.php
@@ -4,39 +4,22 @@ declare(strict_types=1);
 
 namespace Selector\Admin\GutenbergEditor;
 
-use MyCLabs\Enum\Enum;
-
 /**
  * Selectors for the block selector
- * 
- * @method static BlockSelector MAIN_BUTTON()
  */
-class BlockSelector extends Enum
+class BlockSelector
 {
-    private const MAIN_BUTTON = '//button[contains(@aria-label, "Add block")]';
-    private const SECTION = '//div[contains(@class, "block-editor-block-types-list")][contains(@aria-label, "%s")]';
-    private const BLOCK = '//div[contains(@class, "block-editor-block-types-list")]//button/span[text()="%s"]';
+    public const MAIN_BUTTON = '//button[contains(@aria-label, "Add block")]';
+    public const SECTION = '//div[contains(@class, "block-editor-block-types-list")][contains(@aria-label, "%s")]';
+    public const BLOCK = '//div[contains(@class, "block-editor-block-types-list")]//button/span[text()="%s"]';
 
-    public static function SECTION(BlockSection $sectionName): self
+    public static function section(BlockSection $sectionName): string
     {
-        return new BlockSelector(sprintf(self::SECTION, (string) $sectionName));
+        return sprintf(self::SECTION, (string) $sectionName);
     }
 
-    public static function BLOCK(BlockName $blockName): self
+    public static function block(BlockName $blockName): string
     {
-        return new BlockSelector(sprintf(self::BLOCK, (string) $blockName));
-    }
-
-    /**
-     * Check if is valid enum value
-     * Overwrites Enum method to allow for parameters in const
-     *
-     * @param $value
-     * @see Enum::isValid()
-     * @return bool
-     */
-    public static function isValid($value): bool
-    {
-        return true;
+        return sprintf(self::BLOCK, (string) $blockName);
     }
 }
