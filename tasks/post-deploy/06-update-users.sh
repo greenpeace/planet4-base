8a6c36decfd48fe5f50a80091f32ad75ab646525
Nikos Roussos <nikos@roussos.cc>
2021-07-13 13:40:55 +0300

    Update user permissions

--- tasks/post-deploy/06-update-users.sh
+++ tasks/post-deploy/06-update-users.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wp user create dtovbein dtovbein@greenpeace.org --first_name="Dan" --last_name="Tovbein - P4 team" --role=editor --porcelain || true
+wp user create dtovbein dtovbein@greenpeace.org --first_name="Dan" --last_name="Tovbein - P4 team" --role=administrator --porcelain || true
 wp user delete nhazim@greenpeace.org --reassign=1 --yes
