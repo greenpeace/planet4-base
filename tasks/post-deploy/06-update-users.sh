c298c2c1d86627982f1a78e711c6ad26c10d5cf9
Nikos Roussos <nikos@roussos.cc>
2021-10-20 18:38:48 +0300

    Add env check for P4 users

--- tasks/post-deploy/06-update-users.sh
+++ tasks/post-deploy/06-update-users.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wp user create dtovbein tdhooper@greenpeace.org --first_name="Taylor" --last_name="Kaus - P4 team" --role=editor --porcelain || true
+if [ "$APP_ENV" = "development" ]; then
+  wp user create dtovbein tdhooper@greenpeace.org --first_name="Taylor" --last_name="Kaus - P4 team" --role=administrator --porcelain || true
+fi
