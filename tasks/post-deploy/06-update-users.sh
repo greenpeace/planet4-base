4b69046a7eaab1b23fe050abef56982ecc7382f5
Nikos Roussos <nikos@roussos.cc>
2021-10-20 18:57:26 +0300

    Fix users script

--- tasks/post-deploy/06-update-users.sh
+++ tasks/post-deploy/06-update-users.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$APP_ENV" = "development" ]; then
-  wp user create dtovbein tdhooper@greenpeace.org --first_name="Taylor" --last_name="Kaus - P4 team" --role=administrator --porcelain || true
+  wp user create tdhooper tdhooper@greenpeace.org --first_name="Taylor" --last_name="Kaus - P4 team" --role=administrator --porcelain || true
 fi
