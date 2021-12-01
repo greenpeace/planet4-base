4727bb37f256812107e880581dce835703de93be
Nikos Roussos <nikos@roussos.cc>
2021-12-01 11:35:47 +0200

    PLANET-6484 Enable new navbar on non-customized websites

--- tasks/post-deploy/08-enable-new-navbar.sh
+++ tasks/post-deploy/08-enable-new-navbar.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-# Update new navbar design on dev sites
+# Update new navbar design on all dev sites
 if [ "$APP_ENV" = "development" ]; then
   wp option patch insert planet4_options new_design_country_selector 'on'
   wp option patch insert planet4_options new_design_navigation_bar 'on'
+  exit
 fi
+
+# All code below only runs on production/staging
+
+# Catch NROs on non-gp.org domains
+# ch, ummah, history, handbook, storytelling, gcefca
+if [[ -n "${APP_HOSTPATH}" ]]; then
+  echo "Excluded NRO: ${APP_HOSTNAME}"
+  exit
+fi
+
+# Other NRO websites with customized navbar
+excluded=(hongkong korea taiwan canada argentina japan belgium luxembourg denmark finland sweden norway)
+
+nro="${APP_HOSTPATH}"
+echo "NRO: ${nro}"
+
+for i in "${excluded[@]}"; do
+  if [ "$i" == "$nro" ]; then
+    echo "Excluded NRO"
+    exit
+  fi
+done
+
+wp option patch insert planet4_options new_design_country_selector 'on'
+wp option patch insert planet4_options new_design_navigation_bar 'on'
