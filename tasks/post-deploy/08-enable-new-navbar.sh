52114c0b9c1127fa63af01be94a51c6c2de838ed
Nikos Roussos <nikos@roussos.cc>
2021-12-14 15:30:32 +0200

    Enable new navbar on dev sites

--- tasks/post-deploy/08-enable-new-navbar.sh
+++ tasks/post-deploy/08-enable-new-navbar.sh
@@ -3,4 +3,5 @@
 # Update new navbar design on dev sites
 if [ "$APP_ENV" = "development" ]; then
   wp option patch insert planet4_options new_design_country_selector 'on'
+  wp option patch insert planet4_options new_design_navigation_bar 'on'
 fi
