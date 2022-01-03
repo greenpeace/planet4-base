ddab47027e56f945714859c468d350a21adfa8a5
Pieter Vincent <pvincent@greenpeace.org>
2022-01-03 16:26:18 +0100

    Prevent crash if plugin is not installed

--- tasks/post-deploy/09-disable-wpml-media.sh
+++ tasks/post-deploy/09-disable-wpml-media.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "De-activating WPML Media plugin..."
-wp plugin deactivate wpml-media-translation
+wp plugin is-installed wpml-media-translation && wp plugin deactivate wpml-media-translation
