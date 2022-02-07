1abf98c92eee381a2a8438ed6a2122ed369f8c88
Nikos Roussos <nikos@roussos.cc>
2022-02-07 17:23:49 +0200

    Update default users

--- tasks/post-deploy/06-update-users.sh
+++ tasks/post-deploy/06-update-users.sh
@@ -4,14 +4,13 @@
 # wp user delete <email> --reassign=1 --yes
 
 echo "Sync default users..."
-if [ "$APP_ENV" = "development" ]; then
-  wp user delete jhasenau@greenpeace.org --reassign=1 --yes
-  wp user delete aradu@greenpeace.org --reassign=1 --yes
-  wp user delete tdhooper@greenpeace.org --reassign=1 --yes
-  wp user delete lhillige@greenpeace.org --reassign=1 --yes
-  wp user delete ltitus@greenpeace.org --reassign=1 --yes
-  wp user delete tzetterl@greenpeace.org --reassign=1 --yes
-fi
+wp user delete jhasenau@greenpeace.org --reassign=1 --yes
+wp user delete aradu@greenpeace.org --reassign=1 --yes
+wp user delete tdhooper@greenpeace.org --reassign=1 --yes
+wp user delete lhillige@greenpeace.org --reassign=1 --yes
+wp user delete ltitus@greenpeace.org --reassign=1 --yes
+wp user delete tzetterl@greenpeace.org --reassign=1 --yes
+wp user delete wmorrisj@greenpeace.org --reassign=1 --yes
 
 wp user create gmintoco gmintoco@greenpeace.org --first_name="Gus" --last_name="Minto Cowcher - P4 team" --role=administrator --porcelain || true
 wp user create tcavalca tcavalca@greenpeace.org --first_name="Tenorio" --last_name="Cavalcante - P4 team" --role=administrator --porcelain || true
