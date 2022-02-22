fd48492b736beda52655f3bc2f6d9543aa07c3d5
Nikos Roussos <nikos@roussos.cc>
2022-02-22 19:35:37 +0200

    License key already base64 decoded

--- tasks/post-deploy/08-gravityforms.sh
+++ tasks/post-deploy/08-gravityforms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Set GF settings..."
-wp option add rg_gforms_key "$(echo "${GF_LICENSE}" | base64 -d)"
+wp option add rg_gforms_key "${GF_LICENSE}"
 wp option add rg_gforms_enable_akismet 1
 wp option add gform_enable_noconflict 1
 wp option add gform_enable_background_updates 0
