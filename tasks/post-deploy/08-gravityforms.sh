4233eac1c95890cb6e95f73260f70d30cbda1b4a
Nikos Roussos <nikos@roussos.cc>
2022-02-22 19:55:29 +0200

    Update license key if option already exists

--- tasks/post-deploy/08-gravityforms.sh
+++ tasks/post-deploy/08-gravityforms.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Set GF settings..."
-wp option add rg_gforms_key "${GF_LICENSE}"
-wp option add rg_gforms_enable_akismet 1
-wp option add gform_enable_noconflict 1
-wp option add gform_enable_background_updates 0
-wp option add rg_gforms_enable_html5 1
+wp option add rg_gforms_key "${GF_LICENSE}" || wp option update rg_gforms_key "${GF_LICENSE}"
+wp option add rg_gforms_enable_akismet 1 || true
+wp option add gform_enable_noconflict 1 || true
+wp option add gform_enable_background_updates 0 || true
+wp option add rg_gforms_enable_html5 1 || true
