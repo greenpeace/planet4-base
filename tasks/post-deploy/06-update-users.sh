cc3ee0ab096ec9b28b5a3478e7da628f519328d7
Nikos Roussos <nikos.roussos@greenpeace.org>
2022-04-14 11:39:47 +0300

    Clean obsolete scripts

--- tasks/post-deploy/06-update-users.sh
+++ tasks/post-deploy/06-update-users.sh
@@ -4,13 +4,4 @@
 # wp user delete <email> --reassign=1 --yes
 
 echo "Sync default users..."
-wp user delete jhasenau@greenpeace.org --reassign=1 --yes
-wp user delete aradu@greenpeace.org --reassign=1 --yes
-wp user delete tdhooper@greenpeace.org --reassign=1 --yes
-wp user delete lhillige@greenpeace.org --reassign=1 --yes
-wp user delete ltitus@greenpeace.org --reassign=1 --yes
-wp user delete tzetterl@greenpeace.org --reassign=1 --yes
-wp user delete wmorrisj@greenpeace.org --reassign=1 --yes
-
-wp user create gmintoco gmintoco@greenpeace.org --first_name="Gus" --last_name="Minto Cowcher - P4 team" --role=administrator --porcelain || true
-wp user create tcavalca tcavalca@greenpeace.org --first_name="Tenorio" --last_name="Cavalcante - P4 team" --role=administrator --porcelain || true
+wp user delete jmarubay@greenpeace.org --reassign=1 --yes
--- tasks/post-deploy/09-sitemap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -n "${APP_HOSTPATH}" ]]; then
-  echo "\"^/${APP_HOSTPATH}/(.*/)sitemap.xml\",\"https://${APP_HOSTNAME}/${APP_HOSTPATH}/\$1wp-sitemap.xml\"" >sitemap.csv
-else
-  echo "\"^/(.*/)sitemap.xml\",\"https://${APP_HOSTNAME}/\$1wp-sitemap.xml\"" >sitemap.csv
-fi
-wp redirection import sitemap.csv --format=csv
