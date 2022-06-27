662b080a263394dfad6e9ddfa2588bd773886779
Nikos Roussos <nikos.roussos@greenpeace.org>
2022-06-27 11:32:50 +0300

    Activate GF plugin by default

--- tasks/post-deploy/08-gravityforms.sh
+++ tasks/post-deploy/08-gravityforms.sh
@@ -7,3 +7,4 @@ wp option add gform_enable_noconflict 1 || true
 wp option add gform_enable_background_updates 0 || true
 wp option add rg_gforms_enable_html5 1 || true
 wp option patch insert stateless-modules gravity-form true || wp option add stateless-modules '{"gravity-form": "true"}' --format=json
+wp plugin activate gravityforms
