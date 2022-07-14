50e35e9fea6880e72fccb714a4435dde1f96ce4f
Nikos Roussos <nikos.roussos@greenpeace.org>
2022-07-14 10:27:10 +0300

    Update users

--- tasks/post-deploy/06-update-users.sh
+++ tasks/post-deploy/06-update-users.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
+echo "Sync default users..."
 # wp user create <username> <email> --first_name="" --last_name=" - P4 team" --role=administrator --porcelain || true
 # wp user delete <email> --reassign=1 --yes
-
-echo "Sync default users..."
-wp user delete jmarubay@greenpeace.org --reassign=1 --yes
-wp user delete pvincent@greenpeace.org --reassign=1 --yes
+wp user create mmarcott mmarcott@greenpeace.org --first_name="Molly" --last_name="Marcott - P4 team" --role=administrator --porcelain || true
