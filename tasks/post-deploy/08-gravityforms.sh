9a5568ea682a9b5ac77caa3037d0e3c40bf4552f
Sagar Deshmukh <sagarsdeshmukh@users.noreply.github.com>
2023-01-26 20:36:51 +0530

    PLANET-6982 Copy GF language translation files (#233)
    Ref. https://jira.greenpeace.org/browse/PLANET-6982
    Copy the GF translation files from Master theme to WP language dir

--- tasks/post-deploy/08-gravityforms.sh
+++ tasks/post-deploy/08-gravityforms.sh
@@ -8,3 +8,7 @@ wp option add gform_enable_background_updates 0 || true
 wp option add rg_gforms_enable_html5 1 || true
 wp option patch insert stateless-modules gravity-form true || wp option add stateless-modules '{"gravity-form": "true"}' --format=json
 wp plugin activate gravityforms
+
+echo "Copy GF language translation files..."
+mkdir -p public/wp-content/languages/gravityforms/
+rsync -ar public/wp-content/themes/planet4-master-theme/languages/plugins/gravityforms/ public/wp-content/languages/gravityforms/
