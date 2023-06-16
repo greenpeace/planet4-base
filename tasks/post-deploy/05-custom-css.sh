e6cf22645855e1210c62d54f5da64a8af8ef96ef
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-06-16 11:39:14 +0300

    Clean up obsolete post-deploy scripts
    No need to clean custom css code anymore.
    It has probably been removed from all websites by now.

--- tasks/post-deploy/05-custom-css.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Deleting Custom CSS code..."
-for p in $(wp post list --post_type=custom_css --field=ID); do
-  wp post update "$p" --post_content=""
-done
--- tasks/post-deploy/06-update-users.sh
+++ tasks/post-deploy/06-update-users.sh
@@ -3,6 +3,4 @@
 echo "Sync default users..."
 # wp user create <username> <email> --first_name="" --last_name=" - P4 team" --role=administrator --porcelain || true
 # wp user delete <email> --reassign=1 --yes
-wp user delete sgrishpu@greenpeace.org --reassign=1 --yes
-wp user delete apapamat@greenpeace.org --reassign=1 --yes
-wp user delete mfatome@greenpeace.org --reassign=1 --yes
+wp user create crangulo crangulo@greenpeace.org --first_name="Carolina" --last_name="Romo Angulo - P4 team" --role=administrator --porcelain || true
