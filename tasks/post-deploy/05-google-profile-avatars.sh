a7edb96ab907fc76c5c73cb8cabc655679a81d76
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-06-29 10:39:22 +0300

    PLANET-7180 Add Google Profile Avatars plugin
    Ref: https://jira.greenpeace.org/browse/PLANET-7180

--- tasks/post-deploy/05-google-profile-avatars.sh
+++ tasks/post-deploy/05-google-profile-avatars.sh
@@ -3,3 +3,4 @@
 echo "Set Google Profile Avatars settings..."
 wp option add gpavatars_options '{"gpa_license_key": ""}' --format=json || true
 wp option patch update gpavatars_options gpa_license_key "${GOOGLE_PROFILES_AVATARS_KEY}"
+wp plugin activate google-profile-avatars
