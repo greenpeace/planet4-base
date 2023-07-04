b15310f40ad7db274813eced81d88b51c4539363
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-07-04 10:39:41 +0300

    Revert "PLANET-7188 Adjust staging robots file"
    This reverts commit 4515b145e19473d61389b44f2a67e694cb2a0b3c.

--- tasks/post-deploy/10-openresty-robots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Override staging robots file..."
-if [ "$APP_ENV" = "staging" ]; then
-  echo "User-agent: *" >source/public/robots.txt
-  echo "Disallow: /" >>source/public/robots.txt
-fi
