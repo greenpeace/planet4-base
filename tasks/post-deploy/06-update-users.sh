163237198f8e080734604ac5e60dc250db6c9458
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-07-13 11:47:32 +0300

    Update user script

--- tasks/post-deploy/06-update-users.sh
+++ tasks/post-deploy/06-update-users.sh
@@ -4,3 +4,4 @@ echo "Sync default users..."
 # wp user create <username> <email> --first_name="" --last_name=" - P4 team" --role=administrator --porcelain || true
 # wp user delete <email> --reassign=1 --yes
 wp user create crangulo crangulo@greenpeace.org --first_name="Carolina" --last_name="Romo Angulo - P4 team" --role=administrator --porcelain || true
+wp user create tgarcia tgarcia@greenpeace.org --first_name="Toni" --last_name="Garcia - P4 team" --role=administrator --porcelain || true
---
+++ tasks/post-deploy/10-new-visual-identity.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Enable new Visual Identity on dev sites..."
+if [ "$APP_ENV" = "development" ]; then
+fi
