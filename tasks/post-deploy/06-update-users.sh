e3a5f12daadf5066d63db1820b222ed7fc588ad6
lithrel <lithrel@randomdomainname.net>
2023-07-27 18:50:51 +0200

    Update user script

--- tasks/post-deploy/06-update-users.sh
+++ tasks/post-deploy/06-update-users.sh
@@ -5,3 +5,4 @@ echo "Sync default users..."
 # wp user delete <email> --reassign=1 --yes
 wp user create crangulo crangulo@greenpeace.org --first_name="Carolina" --last_name="Romo Angulo - P4 team" --role=administrator --porcelain || true
 wp user create tgarcia tgarcia@greenpeace.org --first_name="Toni" --last_name="Garcia - P4 team" --role=administrator --porcelain || true
+wp user create jutorres jutorres@greenpeace.org --first_name="Julia" --last_name="Torres - P4 team" --role=administrator --porcelain || true
