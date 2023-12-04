79af8598edd33ad68a744d64a8490ba364a1a525
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-12-04 16:19:52 +0200

    Remove trailing new line from GF key

--- tasks/post-deploy/08-gravityforms.sh
+++ tasks/post-deploy/08-gravityforms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Set GF settings..."
-wp option update rg_gforms_key "${GF_LICENSE}"
+wp option update rg_gforms_key "${GF_LICENSE%$'\n'}"
 wp option add rg_gforms_enable_akismet 1 || true
 wp option add gform_enable_noconflict 1 || true
 wp option add gform_enable_background_updates 0 || true
