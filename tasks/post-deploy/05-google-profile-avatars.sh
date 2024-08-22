84a9439764d2b77831c5bf1e767fae7f4566639d
Nikos Roussos <nikos.roussos@greenpeace.org>
2024-08-22 12:07:39 +0300

    Remove trailing new line from Google avatars key

--- tasks/post-deploy/05-google-profile-avatars.sh
+++ tasks/post-deploy/05-google-profile-avatars.sh
@@ -2,5 +2,5 @@
 
 echo "Set Google Profile Avatars settings..."
 wp option add gpavatars_options '{"gpa_license_key": ""}' --format=json || true
-wp option patch update gpavatars_options gpa_license_key "${GOOGLE_PROFILE_AVATARS_KEY}"
+wp option patch update gpavatars_options gpa_license_key "${GOOGLE_PROFILE_AVATARS_KEY%$'\n'}"
 wp plugin activate google-profile-avatars
