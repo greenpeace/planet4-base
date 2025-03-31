a929135f4cb7294e4b0cdb36122c3386d95db24a
Nikos Roussos <nikos.roussos@greenpeace.org>
2025-03-31 15:27:53 +0300

    Bumo GF plugins
    Disable no-conflict mode per GF team suggestion

--- tasks/post-deploy/08-gravityforms.sh
+++ tasks/post-deploy/08-gravityforms.sh
@@ -3,7 +3,7 @@
 echo "Set GF settings..."
 wp option update rg_gforms_key "${GF_LICENSE%$'\n'}"
 wp option add rg_gforms_enable_akismet 1 || true
-wp option add gform_enable_noconflict 1 || true
+wp option add gform_enable_noconflict 0 || true
 wp option add gform_enable_background_updates 0 || true
 wp option add rg_gforms_enable_html5 1 || true
 wp option patch insert stateless-modules gravity-form true || wp option add stateless-modules '{"gravity-form": "true"}' --format=json
