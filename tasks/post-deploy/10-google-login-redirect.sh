c17c59d409d65b9e86df01a2a0bd83506529a66f
Dan Tovbein <dtovbein@greenpeace.org>
2026-02-02 16:49:55 -0300

    Modify script
    - Turned as a feature

--- tasks/post-deploy/10-google-login-redirect.sh
+++ tasks/post-deploy/10-google-login-redirect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Force to redirect to Google login page..."
-wp eval 'update_option("planet4_options", array_merge(get_option("planet4_options", []), ["enforce_sso" => true]));'
+wp option patch update planet4_features enforce_sso on
