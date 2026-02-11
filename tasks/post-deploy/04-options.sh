9e9bc918e0bd86b3561cbd5770f603fa4ede10b9
Dan Tovbein <dtovbein@greenpeace.org>
2026-02-11 10:17:38 -0300

    Move script to options script

--- tasks/post-deploy/04-options.sh
+++ tasks/post-deploy/04-options.sh
@@ -2,3 +2,6 @@
 
 echo "Disable Attachment Pages..."
 wp option set wp_attachment_pages_enabled 0
+
+echo "Force to redirect to Google login page..."
+wp option patch insert planet4_features enforce_sso on
--- tasks/post-deploy/10-google-login-redirect.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Force to redirect to Google login page..."
-wp option patch insert planet4_features enforce_sso on
-
