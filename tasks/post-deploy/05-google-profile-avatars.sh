08ad1b07979d01b65fb1f5ede6903c9577dcd9c3
Nikos Roussos <nikos.roussos@greenpeace.org>
2026-03-19 12:39:31 +0200

    Upgrade Google Profile Avatars to 1.6.0
    Better compatibility with WP 6.9.
    
    They changed the zip filename, so we need to do it in two steps
    and deactivate the old plugin first.
    
    Settings and option keys are preserved.

--- tasks/post-deploy/05-google-profile-avatars.sh
+++ tasks/post-deploy/05-google-profile-avatars.sh
@@ -3,4 +3,7 @@
 echo "Configure Google Profile Avatars plugin..."
 wp option add gpavatars_options '{"gpa_license_key": ""}' --format=json || true
 wp option patch update gpavatars_options gpa_license_key "${GOOGLE_PROFILE_AVATARS_KEY%$'\n'}"
-wp plugin activate google-profile-avatars
+
+# Temporary fix for upgrading the plugin with new zip filename
+wp plugin deactivate google-profile-avatars
+wp plugin install --activate https://storage.googleapis.com/planet4-3rdparty-plugins/googleprofileavatar-premium-1.6.0.zip
